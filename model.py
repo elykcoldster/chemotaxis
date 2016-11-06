e1c9afb7434c74dc4da374fae374d190dbd7dc7f
Amir Alavi <s.amir.alavi@gmail.com>
2016-11-06 13:01:32 -0500

    Parameterized the singleton get_instance fcn

--- model.py
+++ model.py
@@ -4,26 +4,25 @@ class Model:
     # A reference to the singleton object
     __instance = None
 
-    def __init__(self):
+    def __init__(self, dt, source_position, source_strength, source_decay_rate):
         """Model ctor, not meant to be called by the user
 
         By convention, always call get_instance() when you want a valid
         reference to the singleton Model. Never call the constructor
         explicitly.
         """
-        # TODO: provide methods to set these to different values later
         self.time = 0
-        self.dt = 0.1
-        self.source_position = np.array([0,0])
-        self.source_strength = 1
-        self.source_decay_rate = 1
+        self.dt = dt
+        self.source_position = source_position
+        self.source_strength = source_strength
+        self.source_decay_rate = source_decay_rate
         self.larvae = []
         # TODO: add other model objects and params (arena, etc)
     
     @staticmethod
-    def get_instance():
+    def get_instance(dt=0.1, source_position=np.array([0,0]), source_strength=1, source_decay_rate=1):
         if not Model.__instance:
-            Model.__instance = Model()
+            Model.__instance = Model(dt, source_position, source_strength, source_decay_rate)
         return Model.__instance
 
     def add_larva(self, l):
